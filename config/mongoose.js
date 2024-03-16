b658ca706b965148ed4d8e64801bc7ead4613bbc
sanket622 <sanketkumar0068@gmail.com>
2024-03-16 23:18:10 +0530

    updates

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,29 @@
+// IMPORTING PACKAGE
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://localhost:27017/habit_tracker")
-  .then(() => console.log("Mongodb Connected"))
-  .catch((e) => console.log(e));
-  
+
+// MAKING CONNECTION 
+const DB = mongoose.connect('mongodb+srv://himadrinayak:12345@cluster0.h7n86ah.mongodb.net/csv-upload?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+// Handle connection status
+DB.then(() => {
+    console.log('Connection successful!');
+}).catch((err) => {
+    console.log('No connection:', err);
+});
+
+
+// Setting it to db
+const db = mongoose.connection;
+
+
+// CHECKING CONNECTION
+//if error occurs
+db.on("error", console.error.bind(console, "Error connecting to DB"));
+// when db connects successfully
+db.once("open", function(){
+    console.log("Successfully connected to DB");
+});
+
+module.exports = db;
